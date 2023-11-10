497e1b543abd3704f5b01dbe6eecffcac7c2eec6
kwp-imac <parkkw09@gmail.com>
2023-11-10 23:05:48 +0900

    Initial Commit

---
+++ ObservableDemo/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  ObservableDemo
+//
+//  Created by kwp-imac on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
